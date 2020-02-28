d52485d7292ae44e7ae532aab2fe5b541e228a6d
Sreenath Devireddy <sreenathreddy9194@gmail.com>
2020-02-28 12:39:35 -0800

    Update Combine_data.py

--- Combine_data.py
+++ Combine_data.py
@@ -1,14 +1,24 @@
+import os
+import pandas as pd
+
 # input_dir_dvv is the file location for dvv data folder
 # input_dir_dv is the file location for dv data folder
 
-dvv_files = [f for f in listdir(input_dir_dvv) if isfile(join(input_dir_dvv, f))]
-dv_files = [f for f in listdir(input_dir_dv) if isfile(join(input_dir_dv, f))]
+# Replace the 'CELL TYPE' with individual folder names Eg : Cell_A, Mixed_Cell_Type_1
 
+# Example: input_dir_dvv = "/home/user/code/AnalyzeDataSet2-master/cell_A/DVV"
+
+input_dir_dvv = ".../AnalyzeDataSet2-master/{CELL TYPE}/DVV"
+input_dir_dv = ".../AnalyzeDataSet2-master/{CELL TYPE}//DV"
+
+dvv_files = [f for f in os.listdir(input_dir_dvv)]
+dv_files = [f for f in os.listdir(input_dir_dv)]
 
 for dvv_f, dv_f in zip(dvv_files, dv_files):
 
-    dvv_df = pd.read_csv(input_dir_dvv + dvv_f)
-    dv_df = pd.read_csv(input_dir_dv + dv_f)
+    dvv_df = pd.read_csv(input_dir_dvv + "/" + dvv_f)
+    dv_df = pd.read_csv(input_dir_dv + "/" + dv_f)
+
 
     # Here we combine the data of 106999 frequency dvv data with 27999999 dv data
     dvv_df = dvv_df[["real_time_106999","real_data_106999","imag_data_106999"]]
@@ -19,8 +29,3 @@ for dvv_f, dv_f in zip(dvv_files, dv_files):
 
     # Use the combined df data to proceed with analysis. 
     combined_df = pd.concat([dvv_df,dv_df], axis=1, sort=False)
-
-
-
-
-
